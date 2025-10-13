b898bad91814c6e371aa4ced903f180ff5215559
Alex Buckley <alexmeb81@outlook.com>
2025-10-13 00:56:45 -0700

    Cleaned up and formatted code with comments

--- src/PacketDecode1.c
+++ src/PacketDecode1.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// MAC Address Format
 #define MAC_ADDR_LEN 6
 #define MAC_ADDR_DELIM ":"
+
+// Type Field Format
 #define TYPE_LEN 2
 #define TYPE_DELIM ""
+
+// Payload Format
 #define PAYLOAD_DELIM " "
 #define PAYLOAD_COL_DELIM "\t"
 #define PAYLOAD_ROW_DELIM "\n"
@@ -14,8 +18,10 @@
 #define PAYLOAD_NUM_COLS 4
 
 
-
-static inline int printBytes(FILE* file, int numBytes, char* delim) {
+// Prints specified number of bytes pointed to by `file` arg
+// Output is separated by `delim` arg
+// Does not check for end of file or error after reading bytes
+int printBytes(FILE* file, int numBytes, const char* delim) {
     int nextByte = 0;
     int bytesRead = 0;
 
@@ -26,6 +32,7 @@ static inline int printBytes(FILE* file, int numBytes, char* delim) {
         printf("%02X%s", nextByte& 0xFF, delim);
     }
 
+    // Print last byte without delimiter
     fread(&nextByte, 1, 1, file);
     bytesRead++;
     printf("%02X", nextByte& 0xFF);
@@ -34,34 +41,46 @@ static inline int printBytes(FILE* file, int numBytes, char* delim) {
 }
 
 
-static inline int printBytesSafe(FILE* file, int numBytes, char* delim) {
-    int nextByte = 0;
-    int bytesRead = 0;
+// Prints next byte pointed to by FILE* arg
+// Returns non-zero if end of file reached
+static inline int printByteSafe(FILE* file) {
+    int nextByte;
 
-    while(bytesRead < numBytes - 1) {
-        fread(&nextByte, 1, 1, file);
+    // Read and display byte
+    fread(&nextByte, 1, 1, file);
+    printf("%02X", nextByte& 0xFF);
 
-        if(feof(file)) // Halt reading
-            break;
+    return feof(file); 
+}
 
-        bytesRead++;
-        printf("%02X%s", nextByte& 0xFF, delim);
-    }
 
-    // Read last byte while end of file not reached
-    if(bytesRead == numBytes - 1) {
-        fread(&nextByte, 1, 1, file);
+// Prints the payload portion of an Ethernet packet
+// Prints in the column-based format specified by PAYLOAD_ Macro constants
+// FILE* argument must point to begining of payload data
+// Returns the number of bytes read
+int printPayload(FILE* file) {
+    int bytesRead = 0; // Tracks total bytes read
+    int rowLen = PAYLOAD_NUM_COLS * PAYLOAD_COL_WIDTH; // Total row length
 
-        if(!feof(file)) { // Last byte read successfully
-            bytesRead++;
-            printf("%02X", nextByte& 0xFF);
+    // Print bytes until end of file
+    while(!printByteSafe(file)) {
+        if(bytesRead % rowLen == 0) { // End of row reached
+            printf(PAYLOAD_ROW_DELIM);
+        } else if(bytesRead % PAYLOAD_COL_WIDTH == 0) { // End of column reached
+            printf(PAYLOAD_COL_DELIM);
+        } else { // Use standard delimiter
+            printf(PAYLOAD_DELIM);
         }
+
+        bytesRead++; // Increment count
     }
 
     return bytesRead;
 }
 
 
+// Run program to decode and display Ethernet packets
+// Takes path to .bin file containing one packet of data as argument
 int main(int argc, char *argv[]) {
     int errCode = 0; // Tracks errors
     int nextByte = 0; // Holds byte being processed
@@ -70,7 +89,7 @@ int main(int argc, char *argv[]) {
 
     if(argc < 2) { // No filepath argument received
         errCode = 1;
-    } else {
+    } else { // Attempt to open binary packet data
         packetData = fopen(argv[1], "rb"); // Open file
 
         if(!packetData) { // Could not open file
@@ -80,29 +99,19 @@ int main(int argc, char *argv[]) {
 
             // Print destination MAC address    
             printf("\nDestination MAC address:\t\t\t");
-            if(printBytes(packetData, MAC_ADDR_LEN, MAC_ADDR_DELIM) != MAC_ADDR_LEN) 
-                errCode = 3; // Dest MAC address failed to read
-
+            printBytes(packetData, MAC_ADDR_LEN, MAC_ADDR_DELIM);
+        
             // Print Source MAC address
             printf("\nSource MAC address:\t\t\t\t");
-            if(printBytes(packetData, MAC_ADDR_LEN, MAC_ADDR_DELIM) != MAC_ADDR_LEN)
-                errCode = 3; // Source MAC address failed to read
+            printBytes(packetData, MAC_ADDR_LEN, MAC_ADDR_DELIM);
             
             // Print Type
             printf("\nType:\t\t\t\t\t\t\t");
-            if(printBytes(packetData, TYPE_LEN, TYPE_DELIM) != TYPE_LEN)
-                errCode = 3; // Type failed to read
+            printBytes(packetData, TYPE_LEN, TYPE_DELIM);
 
+            // Print payload
             printf("\n\nPayload:\n");
-            
-            // Print payload until end of file
-            while(1) {
-                printBytesSafe(packetData, 8, " ");
-                printf("\t");
-                printBytesSafe(packetData, 8, " ");
-                
-            }
-
+            printPayload(packetData);
         }
 
     }
